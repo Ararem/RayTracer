69bf390eb579e472567f4cf6441cca2398dc1d61
Eternal <rowanradosavmcrae@gmail.com>
2022-03-30 22:15:19 +1000

    All render options seem to be working correctly

--- RayTracer.Display.EtoForms/RenderOptionSelector.cs
+++ RayTracer.Display.EtoForms/RenderOptionSelector.cs
@@ -1,9 +1,10 @@
 using Eto.Forms;
 using JetBrains.Annotations;
 using RayTracer.Core.Graphics;
+using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -22,16 +23,25 @@ internal class RenderOptionSelector : Panel, INotifyPropertyChanged
 		foreach (PropertyInfo prop in typeof(RenderOptions).GetProperties())
 		{
 			PropertyEditorView view;
-			Label              label = new() {ID = $"{prop.Name} label", Text = prop.Name };
+			Label              label = new() { ID = $"{prop.Name} label", Text = prop.Name };
 
 			TableCell labelCell  = new(label);
 			TableCell editorCell = new();
-			tableLayout.Rows!.Add(new TableRow(labelCell, editorCell));
+			tableLayout.Rows!.Add(new TableRow(labelCell, editorCell){ScaleHeight = false});
 
 			//Switch to create the editor
 			if (prop.PropertyType == typeof(int))
 			{
 				view = new IntEditor(this, prop, editorCell);
+			}
+			else if (prop.PropertyType == typeof(float))
+			{
+				view = new FloatEditor(this, prop, editorCell);
+			}
+			else if (prop.PropertyType.IsEnum)
+			{
+				//Hey it's a little funky but it works
+				view = (PropertyEditorView)Activator.CreateInstance(typeof(EnumEditor<>).MakeGenericType(prop.PropertyType), this, prop, editorCell)!;
 			}
 			else
 			{
@@ -41,6 +51,7 @@ internal class RenderOptionSelector : Panel, INotifyPropertyChanged
 
 			propertyEditors.Add(view);
 		}
+		tableLayout.Rows!.Add(new TableRow()); //Add empty row so that scaling looks nice (last row sizes to fil gap)
 
 		UpdateSubViews();
 	}
@@ -54,12 +65,12 @@ internal class RenderOptionSelector : Panel, INotifyPropertyChanged
 		/// <inheritdoc/>
 		public IntEditor(RenderOptionSelector target, PropertyInfo prop, TableCell tableCell) : base(target, prop, tableCell)
 		{
-			int min = int.MinValue;
-			int max = int.MaxValue;
-			if (Prop.GetCustomAttribute<NonNegativeValueAttribute>() is not null)
+			int       min          = int.MinValue;
+			int       max          = int.MaxValue;
+			if (typeof(RenderOptions).GetConstructors()[0].GetParameters().FirstOrDefault(p=>p.Name ==prop.Name)?.GetCustomAttribute<NonNegativeValueAttribute>() is not null)
 				min = 0;
 
-			stepper              =  new NumericStepper { ID = $"{Prop.Name} stepper", Increment = 1, MaximumDecimalPlaces = 0, MinValue = min, MaxValue = max};
+			stepper              =  new NumericStepper { ID = $"{Prop.Name} stepper", Increment = 1, MaximumDecimalPlaces = 0, MinValue = min, MaxValue = max };
 			stepper.ValueChanged += (sender, _) => Prop.SetValue(Target.Options, (int)((NumericStepper)sender!).Value);
 			TableCell.Control    =  stepper;
 		}
@@ -68,6 +79,53 @@ internal class RenderOptionSelector : Panel, INotifyPropertyChanged
 		internal override void UpdateDisplayedFromTarget()
 		{
 			stepper.Value = (int)Prop.GetValue(Target.Options)!;
+		}
+	}
+	private sealed class EnumEditor<T> : PropertyEditorView
+	{
+		private readonly EnumDropDown<T> dropDown;
+
+		/// <inheritdoc/>
+		public EnumEditor(RenderOptionSelector target, PropertyInfo prop, TableCell tableCell) : base(target, prop, tableCell)
+		{
+			dropDown                      =  new EnumDropDown<T> { ID = $"{Prop.Name} dropdown"};
+			dropDown.SelectedValueChanged += (sender, _) => Prop.SetValue(Target.Options, ((EnumDropDown<T>)sender!).SelectedValue);
+			TableCell.Control             =  dropDown;
+		}
+
+		/// <inheritdoc/>
+		internal override void UpdateDisplayedFromTarget()
+		{
+			dropDown.SelectedValue = (T)Prop.GetValue(Target.Options)!;
+		}
+	}
+
+	private sealed class FloatEditor : PropertyEditorView
+	{
+		private readonly NumericStepper stepper;
+
+		/// <inheritdoc/>
+		public FloatEditor(RenderOptionSelector target, PropertyInfo prop, TableCell tableCell) : base(target, prop, tableCell)
+		{
+			float     min          = float.MinValue;
+			float     max          = float.MaxValue;
+			// //It's a record so must have a backing field, don't have to worry about null refs
+			// FieldInfo backingField = prop.DeclaringType!.GetField($"<{Prop.Name}>k__BackingField", BindingFlags.Instance | BindingFlags.NonPublic)!;
+			// if ((Prop.GetCustomAttribute<NonNegativeValueAttribute>() ?? backingField.GetCustomAttribute<NonNegativeValueAttribute>()) is not null)
+			//Honestly why the hell do I have to do this microsoft????
+			//Just apply it to the field, property and parameter, please for the love of god
+			if (typeof(RenderOptions).GetConstructors()[0].GetParameters().FirstOrDefault(p=>p.Name==prop.Name)?.GetCustomAttribute<NonNegativeValueAttribute>() is not null)
+				min = 0f;
+
+			stepper              =  new NumericStepper { ID = $"{Prop.Name} stepper", Increment = 1, MinValue = min, MaxValue = max };
+			stepper.ValueChanged += (sender, _) => Prop.SetValue(Target.Options, (float)((NumericStepper)sender!).Value);
+			TableCell.Control    =  stepper;
+		}
+
+		/// <inheritdoc/>
+		internal override void UpdateDisplayedFromTarget()
+		{
+			stepper.Value = (float)Prop.GetValue(Target.Options)!;
 		}
 	}
 
@@ -82,8 +140,8 @@ internal class RenderOptionSelector : Panel, INotifyPropertyChanged
 	{
 		protected PropertyEditorView(RenderOptionSelector target, PropertyInfo prop, TableCell tableCell)
 		{
-			Target          = target;
-			Prop            = prop;
+			Target    = target;
+			Prop      = prop;
 			TableCell = tableCell;
 		}
 
