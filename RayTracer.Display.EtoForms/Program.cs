196645215fc78498a4311a705ca10f863ae56d87
Eternal <rowanradosavmcrae@gmail.com>
2022-03-31 13:51:54 +1000

    Revert "Something broke"
    This reverts commit 2a8b6ca1582812a0295ca7480ffa77bd446c91c3.

--- RayTracer.Display.EtoForms/Program.cs
+++ RayTracer.Display.EtoForms/Program.cs
@@ -1,48 +1,23 @@
 ﻿using Eto;
 using Eto.Forms;
-using RayTracer.Display.EtoForms;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
 using System;
-using static Serilog.Log;
-using Logger = RayTracer.Core.Logger;
+
+namespace RayTracer.Display.EtoForms;
 
 internal class Program
 {
 	[STAThread]
-	private static int Main(string[] args)
+	private static void Main(string[] args)
 	{
-		Logger.Init();
-		Information("Commandline args: {Args}", args);
+		// ReSharper disable AssignNullToNotNullAttribute
+		Log.Logger = new LoggerConfiguration()
+					.WriteTo.Console(applyThemeToRedirectedOutput: true, theme: AnsiConsoleTheme.Code)
+					.CreateLogger();
+		Log.Information("Logger Initialized");
+		// ReSharper restore AssignNullToNotNullAttribute
 
-		Platform platform;
-		try
-		{
-			Verbose("Getting platform");
-			platform = Platform.Detect!;
-			Verbose("Got Platform");
-		}
-		catch (Exception e)
-		{
-			Fatal(e, "Could not initialise Eto.Forms platform");
-			return -1;
-		}
-		// Verbose("Platform is {Platform}", platform);
-		MainForm form;
-		try
-		{
-			Verbose("Creating MainForm");
-			form = new MainForm();
-			Verbose("Created MainForm");
-		}
-		catch (Exception e)
-		{
-			Fatal(e, "Could not initialise MainForm");
-			return -1;
-		}
-		// Verbose("MainForm is {MainForm}", form);
-
-
-		Information("Running App with ");
-		new Application(platform).Run(form);
-		return 0;
+		new Application(Platform.Detect!).Run(new MainForm());
 	}
 }
