d3ecae40f0c5965d0c7423b6922da9ee52706dfe
Eternal <rowanradosavmcrae@gmail.com>
2022-04-11 12:03:52 +1000

    Try and create a box courtesy of IQ

--- RayTracer.Core/Hittables/Box.cs
+++ RayTracer.Core/Hittables/Box.cs
@@ -3,57 +3,110 @@ using static System.MathF;
 
 namespace RayTracer.Core.Hittables;
 
-public record Box(Vector3 Min, Vector3 Max) : Hittable
+public record Box(Vector3 Min, Vector3 Max, Matrix4x4 WorldToBoxTransform) : Hittable
 {
-	private readonly Lazy<Vector3> centre = new(() => (Min + Max) / 2f);
+	private readonly Lazy<Matrix4x4> boxToWorldTransform = new(
+			() =>
+			{
+				Matrix4x4.Invert(WorldToBoxTransform, out Matrix4x4 result);
+				return result;
+			}
+	);
 
-	/// <inheritdoc />
+	private readonly Lazy<Vector3> centre     = new(() => (Min + Max)          / 2f);
+	private readonly Lazy<float>   halfLength = new(() => (Max - Min).Length() / 2f);
+
+	/// <inheritdoc/>
 	public override HitRecord? TryHit(Ray ray, float kMin, float kMax)
 	{
-		//This is slightly modified code from the ray intersection of an AABB
-		float   k =float.PositiveInfinity;
+		// Calcs intersection and exit distances, normal, face and UVs
+		// row is the ray origin in world space
+		// rdw is the ray direction in world space
+		// txx is the world-to-box transformation
+		// txi is the box-to-world transformation
+		// ro and rd are in world space
+		// rad is the half-length of the box
+		//
+		// oT contains the entry and exit points
+		// oN is the normal in world space
+		// oU contains the UVs at the intersection point
+		// oF contains the index if the intersected face [0..5]
+
+		// convert from world to box space
+
+		Vector4 temp4 = Vector4.Transform(new Vector4(ray.Direction, 0f), WorldToBoxTransform);
+		Vector3 rd    = new(temp4.X, temp4.Y, temp4.Z);
+		temp4 = Vector4.Transform(new Vector4(ray.Origin, 1f), WorldToBoxTransform);
+		Vector3 ro = new(temp4.X, temp4.Y, temp4.Z);
+
+		// ray-box intersection in box space
+		Vector3 m = new Vector3(1f) / rd;
+		Vector3 s = new(
+				rd.X < 0f ? 1f : -1f,
+				rd.Y < 0f ? 1f : -1f,
+				rd.Z < 0f ? 1f : -1f
+		);
+		Vector3 t1 = m * (-ro + (s * halfLength.Value));
+		Vector3 t2 = m * (-ro - (s * halfLength.Value));
+
+		float kNear = Max(Max(t1.X, t1.Y), t1.Z);
+		float kFar  = Min(Min(t2.X, t2.Y), t2.Z);
+
+		//Validate our K value ranges
+		if ((kNear > kFar) || (kFar < 0f)) return null;
+		float k = kNear;
+		if ((k < kMin) || (kMax < k))
+		{
+			k = kFar;
+			if ((k < kMin) || (kMax < k)) return null;
+		}
+
+		// compute normal (in world space), face and UV
 		Vector3 normal;
+		Vector2 uv;
+		int     faceIndex;
+		if ((t1.X > t1.Y) && (t1.X > t1.Z))
 		{
-			float invD = 1.0f                         / ray.Direction.X;
-			float t0   = (Min.X - ray.Origin.X) * invD;
-			float t1   = (Max.X - ray.Origin.X) * invD;
-			if (invD < 0.0f)
-				(t0, t1) = (t1, t0); //Swap
-			float tMin = t0 > kMin ? t0 : kMin;
-			float tMax = t1 < kMax ? t1 : kMax;
-			if (tMax <= tMin)
-				return null;
-			k = Min(k, tMin);
-			normal = ray.Origin.X <
+			Vector3 v = new(
+					boxToWorldTransform.Value.M11,
+					boxToWorldTransform.Value.M12,
+					boxToWorldTransform.Value.M13
+			);
+			normal    = v * s.X;
+			uv        = new Vector2(ro.Y, ro.Z) + (new Vector2(rd.Y, rd.Z) * t1.X);
+			faceIndex = (1 + (int)s.X) / 2;
 		}
+		else if (t1.Y > t1.Z)
 		{
-			float invD = 1.0f                         / ray.Direction.Y;
-			float t0   = (Min.Y - ray.Origin.Y) * invD;
-			float t1   = (Max.Y - ray.Origin.Y) * invD;
-			if (invD < 0.0f)
-				(t0, t1) = (t1, t0); //Swap
-			float tMin = t0 > kMin ? t0 : kMin;
-			float tMax = t1 < kMax ? t1 : kMax;
-			if (tMax <= tMin)
-				return null;
-			k = Min(k, tMin);
+			Vector3 v = new(
+					boxToWorldTransform.Value.M21,
+					boxToWorldTransform.Value.M22,
+					boxToWorldTransform.Value.M23
+			);
+			normal    = v * s.Y;
+			uv        = new Vector2(ro.Z, ro.Z) + (new Vector2(rd.Z, rd.X) * t1.Y);
+			faceIndex = (5 + (int)s.Y) / 2;
 		}
+		else
 		{
-			float invD = 1.0f                         / ray.Direction.Z;
-			float t0   = (Min.Z - ray.Origin.Z) * invD;
-			float t1   = (Max.Z - ray.Origin.Z) * invD;
-			if (invD < 0.0f)
-				(t0, t1) = (t1, t0); //Swap
-			float tMin = t0 > kMin ? t0 : kMin;
-			float tMax = t1 < kMax ? t1 : kMax;
-			if (tMax <= tMin)
-				return null;
-			k = Min(k, tMin);
+			Vector3 v = new(
+					boxToWorldTransform.Value.M31,
+					boxToWorldTransform.Value.M32,
+					boxToWorldTransform.Value.M33
+			);
+			normal    = v * s.Z;
+			uv        = new Vector2(ro.X, ro.Y) + (new Vector2(rd.X, rd.Y) * t1.Z);
+			faceIndex = (9 + (int)s.Z) / 2;
 		}
 
 		Vector3 worldPoint = ray.PointAt(k);
-		Vector3 localPoint      = worldPoint - centre.Value;
+		Vector3 localPoint = worldPoint - centre.Value;
 
-		return new HitRecord(ray, worldPoint, localPoint,);
+		//
+		_ = uv;
+		_ = faceIndex;
+
+		//TODO: UV's, outside face
+		return new HitRecord(ray, worldPoint, localPoint, normal, k, Vector3.Dot(ray.Direction, normal) < 0f, Vector2.Zero);
 	}
 }
