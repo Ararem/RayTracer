8e9529baf3ff1808e92570d7b14ad36c5be6368a
Eternal <rowanradosavmcrae@gmail.com>
2022-04-12 19:17:18 +1000

    Create base light class

---
+++ RayTracer.Core/Environment/Light.cs
@@ -0,0 +1,19 @@
+using RayTracer.Core.Hittables;
+
+namespace RayTracer.Core.Environment;
+
+/// <summary>
+/// Base class that defines a light that can be used to calculate lighting more accurately in the scene
+/// </summary>
+public abstract record Light
+{
+
+	/// <summary>
+	/// Calculates the light emitted by the current <see cref="Light"/> instance, for the hit stored in the <paramref name="hit"/>
+	/// </summary>
+	/// <param name="hit">The information about the hit to calculate the lighting for</param>
+	/// <param name="findClosestIntersection">A function that can be used to calculate the closest intersection along a given ray</param>
+	/// <returns>The amount of light received by the <paramref name="hit"/>, from this light source</returns>
+	/// <remarks>Use <see cref="findClosestIntersection"/> to detect whether there are objects in between the target object, and the light source.</remarks>
+	public abstract Colour CalculateLight(HitRecord hit, Func<Ray, (SceneObject sceneObject, HitRecord hit)?> findClosestIntersection);
+}
--- RayTracer.Core/Lighting/Light.cs
+++
@@ -1,5 +0,0 @@
-using System.Numerics;
-
-namespace RayTracer.Core.Lighting;
-
-public record Light(Vector3 Position);
