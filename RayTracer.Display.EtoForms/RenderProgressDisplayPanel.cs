fa49e7a7a5a7d1079b997d62b1ab20dd2e9d4816
Eternal <rowanradosavmcrae@gmail.com>
2022-04-23 21:41:02 +1000

    Revamp render progress display panel

--- RayTracer.Display.EtoForms/RenderProgressDisplayPanel.cs
+++ RayTracer.Display.EtoForms/RenderProgressDisplayPanel.cs
@@ -9,9 +9,12 @@ using System;
 using System.Buffers;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Threading;
 using static Serilog.Log;
 using Size = Eto.Drawing.Size;
+
+// using NetFabric.Hyperlinq;
 
 namespace RayTracer.Display.EtoForms;
 
@@ -61,6 +64,11 @@ internal sealed class RenderProgressDisplayPanel : Panel
 	private readonly Timer updatePreviewTimer;
 
 	/// <summary>
+	///  Render stats from the last time we updated the preview
+	/// </summary>
+	private RenderStats prevUpdateStats;
+
+	/// <summary>
 	///  Table that contains the various stats
 	/// </summary>
 	private TableLayout statsTable;
@@ -106,7 +114,7 @@ internal sealed class RenderProgressDisplayPanel : Panel
 
 		//Periodically update the previews using a timer
 		//PERF: This creates quite a few allocations when called frequently
-		updatePreviewTimer = new Timer(static state => Application.Instance.Invoke((Action)state!), UpdateAllPreviews, 0, 500);
+		updatePreviewTimer = new Timer(static state => Application.Instance.Invoke((Action)state!), UpdateAllPreviews, 0, 1000/60);
 	}
 
 	/// <summary>
@@ -115,7 +123,7 @@ internal sealed class RenderProgressDisplayPanel : Panel
 	private void UpdateAllPreviews()
 	{
 		UpdateImagePreview();
-		UpdateStatsTable();
+		UpdateStatsTable(renderJob.RenderStats);
 
 		Invalidate();
 	}
@@ -127,6 +135,8 @@ internal sealed class RenderProgressDisplayPanel : Panel
 		Image<Rgb24>     renderBuffer = renderJob.ImageBuffer;
 		IntPtr           offset       = data.Data;
 		for (int y = 0; y < ySize; y++)
+			//This code assumes the source and dest images are same bit depth and size
+			//Otherwise here be dragons
 			unsafe
 			{
 				Span<Rgb24> renderBufRow = renderBuffer.GetPixelRowSpan(y);
@@ -138,91 +148,137 @@ internal sealed class RenderProgressDisplayPanel : Panel
 			}
 	}
 
-	/// <summary>
-	/// Render stats from the last time we updated the preview
-	/// </summary>
-	private RenderStats prevUpdateStats;
-	private void UpdateStatsTable()
+	private void UpdateStatsTable(RenderStats renderStats)
 	{
-		const string timeFormat     = "h\\:mm\\:ss"; //Format string for Timespan
-		const string dateTimeFormat = "G";           //Format string for DateTime
-		const string percentFormat  = "p1";          //Format string for percentages
-		const string numFormat      = "n0";
-		const int    numAlign       = 15;
-		const int    percentAlign   = 8;
+		List<(string Title, (string Name, string Value)[] NamedValues)> stringStats = new();
 
-		int           totalTruePixels = renderJob.RenderStats.TotalTruePixels;
-		ulong         totalRawPix     = renderJob.RenderStats.TotalRawPixels;
-		ulong         rayCount        = renderJob.RenderStats.RayCount;
-		RenderOptions options         = renderJob.RenderOptions;
-		int           totalPasses     = options.Passes;
-		TimeSpan      elapsed         = renderJob.Stopwatch.Elapsed;
-
-		float    percentageRendered = (float)renderJob.RenderStats.RawPixelsRendered / totalRawPix;
-		ulong    rawPixelsRemaining = totalRawPix - renderJob.RenderStats.RawPixelsRendered;
-		int      passesRemaining    = totalPasses - renderJob.RenderStats.PassesRendered;
-		TimeSpan estimatedTotalTime;
-		//If the percentage rendered is very low, the division results in a number that's too large to fit in a timespan, which throws
-		try
 		{
-			estimatedTotalTime = elapsed / percentageRendered;
+			TimeSpan elapsed = renderJob.Stopwatch.Elapsed;
+			TimeSpan estimatedTotalTime;
+			//If the percentage rendered is very low, the division results in a number that's too large to fit in a timespan, which throws
+			try
+			{
+				estimatedTotalTime = elapsed / ((float)renderStats.RawPixelsRendered / renderStats.TotalRawPixels);
+			}
+			catch (OverflowException)
+			{
+				estimatedTotalTime = TimeSpan.FromDays(69.420); //If something's broke at least let me have some fun
+			}
+
+			stringStats.Add(
+					("Time", new[]
+					{
+							("Elapsed", FormatTime(elapsed)),
+							("Remaining", FormatTime(estimatedTotalTime - elapsed)),
+							("Total", FormatTime(estimatedTotalTime)),
+							("ETC", FormatDate(DateTime.Now + (estimatedTotalTime - elapsed)) + "\t") //We need to add a tab here or else the width keeps changing (thanks non-monospaced fonts!)
+					})
+			);
 		}
-		catch (OverflowException)
 		{
-			estimatedTotalTime = TimeSpan.FromDays(69.420); //If something's broke at least let me have some fun
+			stringStats.Add(("Raw Pixels", new[]
+			{
+					("Rendered",FormatUlongRatio(renderStats.RawPixelsRendered,  renderStats.TotalRawPixels)),
+					("Remaining",FormatUlongRatio(renderStats.TotalRawPixels-renderStats.RawPixelsRendered, renderStats.TotalRawPixels)),
+					("Total",FormatUlong(renderStats.TotalRawPixels)),
+			}));
 		}
-
-		List<(string Title, string[] Values)> stringStats =new()
 		{
-				("Time", new[]
-				{
-						$"{elapsed.ToString(timeFormat),numAlign} elapsed",
-						$"{(estimatedTotalTime - elapsed).ToString(timeFormat),numAlign} remaining",
-						$"{estimatedTotalTime.ToString(timeFormat),numAlign} total",
-						$"{(DateTime.Now + (estimatedTotalTime - elapsed)).ToString(dateTimeFormat),numAlign} ETC"
-				}),
-				("Pixels", new[]
-				{
-						$"{FormatU(renderJob.RenderStats.RawPixelsRendered, totalRawPix)} rendered",
-						$"{FormatU(rawPixelsRemaining,          totalRawPix)} remaining",
-						$"{totalRawPix.ToString(numFormat),numAlign}          total"
-				}),
-				("Image", new[]
-				{
-						$"{totalTruePixels.ToString(numFormat),numAlign}          pixels total",
-						$"{options.Width.ToString(numFormat),numAlign}          pixels wide",
-						$"{options.Height.ToString(numFormat),numAlign}          pixels high"
-				}),
-				("Passes", new[]
-				{
-						$"{FormatI(renderJob.RenderStats.PassesRendered, totalPasses)} rendered",
-						$"{FormatI(passesRemaining,          totalPasses)} remaining",
-						$"{totalPasses.ToString(numFormat),numAlign}          total"
-				}),
-				("Rays", new[]
-				{
-						$"{FormatU(renderJob.RenderStats.RaysScattered,       rayCount)} scattered",
-						$"{FormatU(renderJob.RenderStats.RaysAbsorbed,        rayCount)} absorbed",
-						$"{FormatU(renderJob.RenderStats.BounceLimitExceeded, rayCount)} exceeded",
-						$"{FormatU(renderJob.RenderStats.SkyRays,             rayCount)} sky",
-						$"{rayCount.ToString(numFormat),numAlign}          total"
-				}),
-				("Scene", new[]
-				{
-						$"Name:		{renderJob.Scene.Name}",
-						$"Obj Count:	{renderJob.Scene.SceneObjects.Length}",
-						$"Camera:		{renderJob.Scene.Camera}",
-						$"SkyBox:		{renderJob.Scene.SkyBox}"
-				}),
-				("Renderer", new[]
-				{
-						$"{renderJob.RenderStats.ThreadsRunning} {(renderJob.RenderStats.ThreadsRunning == 1 ? "thread" : "threads")}"
-				})
-		};
+			stringStats.Add(("Image", new[]
+			{
+					//Assumes preview image has same dimensions as render buffer (which should always be the case)
+					("Width",FormatInt(previewImage.Width)),
+					("Height",FormatInt(previewImage.Height)),
+					("Pixels",FormatInt(renderStats.TotalTruePixels)),
+			}));
+		}
+		{
+			stringStats.Add(("Passes", new[]
+			{
+					("Rendered",FormatIntRatio(renderStats.PassesRendered, renderJob.RenderOptions.Passes)),
+					("Remaining",FormatIntRatio(renderStats.PassesRendered, renderJob.RenderOptions.Passes)),
+					("Total",FormatInt(renderJob.RenderOptions.Passes)),
+					("Progress", FormatUlongRatio(renderStats.RawPixelsRendered % (ulong)renderStats.TotalTruePixels, (ulong)renderStats.TotalTruePixels))
+			}));
+		}
+		{
+			stringStats.Add(("Rays", new[]
+			{
+					("Scattered",FormatUlongRatio(renderStats.RaysScattered, renderStats.RayCount)),
+					("Absorbed",FormatUlongRatio(renderStats.RaysAbsorbed, renderStats.RayCount)),
+					("Exceeded",FormatUlongRatio(renderStats.BounceLimitExceeded, renderStats.RayCount)),
+					("Sky",FormatUlongRatio(renderStats.SkyRays, renderStats.RayCount)),
+					("Total",FormatUlong(renderStats.RayCount))
+			}));
+		}
+		{
+			stringStats.Add(("Scene", new[]
+			{
+					("Name",renderJob.Scene.Name),
+					("Object Count",FormatInt(renderJob.Scene.SceneObjects.Length)),
+					("Light Count",FormatInt(renderJob.Scene.Lights.Length)),
+					("Camera",renderJob.Scene.Camera.ToString()!),
+			}));
+		}
+		{
+			stringStats.Add(("Renderer", new[]
+			{
+					("Threads",FormatInt(renderStats.ThreadsRunning)),
+					("Completed",renderJob.RenderCompleted.ToString()),
+					("Task",renderJob.RenderTask.ToString()!),
+			}));
+		}
+		// {
+		// 	("Time", new[]
+		// 	{
+		// 			$"{elapsed.ToString(timeFormat),numAlign} elapsed",
+		// 			$"{(estimatedTotalTime - elapsed).ToString(timeFormat),numAlign} remaining",
+		// 			$"{estimatedTotalTime.ToString(timeFormat),numAlign} total",
+		// 			$"{(DateTime.Now + (estimatedTotalTime - elapsed)).ToString(dateTimeFormat),numAlign} ETC"
+		// 	}),
+		// 	("Pixels", new[]
+		// 	{
+		// 			$"{FormatUlong(renderStats.RawPixelsRendered, totalRawPix)} rendered",
+		// 			$"{FormatUlong(rawPixelsRemaining,                      totalRawPix)} remaining",
+		// 			$"{totalRawPix.ToString(numFormat),numAlign}          total"
+		// 	}),
+		// 	("Image", new[]
+		// 	{
+		// 			$"{totalTruePixels.ToString(numFormat),numAlign}          pixels total",
+		// 			$"{options.Width.ToString(numFormat),numAlign}          pixels wide",
+		// 			$"{options.Height.ToString(numFormat),numAlign}          pixels high"
+		// 	}),
+		// 	("Passes", new[]
+		// 	{
+		// 			$"{FormatInt(renderStats.PassesRendered, totalPasses)} rendered",
+		// 			$"{FormatInt(passesRemaining,                      totalPasses)} remaining",
+		// 			$"{totalPasses.ToString(numFormat),numAlign}          total"
+		// 	}),
+		// 	("Rays", new[]
+		// 	{
+		// 			$"{FormatUlong(renderStats.RaysScattered,       rayCount)} scattered",
+		// 			$"{FormatUlong(renderStats.RaysAbsorbed,        rayCount)} absorbed",
+		// 			$"{FormatUlong(renderStats.BounceLimitExceeded, rayCount)} exceeded",
+		// 			$"{FormatUlong(renderStats.SkyRays,             rayCount)} sky",
+		// 			$"{rayCount.ToString(numFormat),numAlign}          total"
+		// 	}),
+		// 	("Scene", new[]
+		// 	{
+		// 			$"Name:		{renderJob.Scene.Name}",
+		// 			$"Obj Count:	{renderJob.Scene.SceneObjects.Length}",
+		// 			$"Camera:		{renderJob.Scene.Camera}",
+		// 			$"SkyBox:		{renderJob.Scene.SkyBox}"
+		// 	}),
+		// 	("Renderer", new[]
+		// 	{
+		// 			$"{renderStats.ThreadsRunning} {(renderStats.ThreadsRunning == 1 ? "thread" : "threads")}"
+		// 	})
+		// }
+		// ;
 
-		//Due to how the table is implemented, I can't rescale it later
+		//Due to how the table is implemented, I can't resize it later
 		//So if the size doesn't match our array, we need to recreate it
-		Size correctDims = new(2, stringStats.Count + 1);
+		Size correctDims = new(3, stringStats.Count + 2); //+2 = 1 for depth + 1 for spacer row
 		if (statsTable.Dimensions != correctDims)
 		{
 			Verbose("Old table dims {Dims} do not match stats array, disposing and recreating with dims {NewDims}", statsTable.Dimensions, correctDims);
@@ -232,37 +288,51 @@ internal sealed class RenderProgressDisplayPanel : Panel
 			statsContainer.Content = statsTable;
 		}
 
-		for (int i = 0; i < stringStats.Count; i++)
+		for (int rowIdx = 0; rowIdx < stringStats.Count; rowIdx++)
 		{
-			(string? title, string[]? strings) = stringStats[i];
-			string   values = StringBuilderPool.BorrowInline(static (sb, vs) => sb.AppendJoin(Environment.NewLine, vs), strings);
-			TableRow row    = statsTable.Rows[i];
+			TableRow row = statsTable.Rows[rowIdx];
+			(string title, (string Name, string Value)[] namedValues) = stringStats[rowIdx];
+			//Aggregate the name and value texts
+			string aggregatedNames  = StringBuilderPool.BorrowInline(static (sb, namedValues) => { sb.AppendJoin(Environment.NewLine, namedValues.Select(val => $"{val.Name}:")); }, namedValues);
+			string aggregatedValues = StringBuilderPool.BorrowInline(static (sb, namedValues) => { sb.AppendJoin(Environment.NewLine, namedValues.Select(val => val.Value)); },      namedValues);
+
 			//Get the Labels at the correct locations, or assign them if needed
 			if (row.Cells[0].Control is not Label titleLabel)
 			{
-				Verbose("Cell [{Position}] was not label, disposing and updating", (0, i));
+				Verbose("Cell [{Position}] was not label (was {Control}), disposing and updating", (0, rowIdx), row.Cells[0].Control);
 				row.Cells[0]?.Control?.Detach();
 				row.Cells[0]?.Control?.Dispose(); //Dispose the old control
-				statsTable.Add(titleLabel = new Label(), 0, i);
+				statsTable.Add(titleLabel = new Label(), 0, rowIdx);
 			}
 
-			if (row.Cells[1].Control is not Label valueLabel)
+			if (row.Cells[1].Control is not Label nameLabel)
 			{
-				Verbose("Cell [{Position}] was not label, disposing and updating", (0, i));
+				Verbose("Cell [{Position}] was not name label (was {Control}), disposing and updating", (1, rowIdx), row.Cells[1].Control);
 				row.Cells[1]?.Control?.Detach();
 				row.Cells[1]?.Control?.Dispose(); //Dispose of the old control
-				statsTable.Add(valueLabel = new Label(), 1, i);
+				statsTable.Add(nameLabel = new Label(), 1, rowIdx);
 			}
 
+			if (row.Cells[2].Control is not Label valueLabel)
+			{
+				Verbose("Cell [{Position}] was not value label (was {Control}), disposing and updating", (2, rowIdx), row.Cells[2].Control);
+				row.Cells[2]?.Control?.Detach();
+				row.Cells[2]?.Control?.Dispose(); //Dispose of the old control
+				statsTable.Add(valueLabel = new Label(), 2, rowIdx);
+			}
+
+
 			titleLabel.Text = title;
-			valueLabel.Text = values;
+			nameLabel.Text  = aggregatedNames;
+			valueLabel.Text = aggregatedValues;
 		}
 
 		{
-			int       row             = statsTable.Dimensions.Height - 1;
+			int       row             = statsTable.Dimensions.Height - 2; //Account for start at 0 offset and last spacer row
 			int       maxDepth        = renderJob.RenderOptions.MaxDepth;
 			TableCell titleCell       = statsTable.Rows[row].Cells[0];
-			TableCell depthBufferCell = statsTable.Rows[row].Cells[1];
+			TableCell descriptionCell = statsTable.Rows[row].Cells[1];
+			TableCell depthBufferCell = statsTable.Rows[row].Cells[2];
 
 			//Update title control type if needed
 			if (titleCell.Control is not Label titleLabel)
@@ -275,13 +345,23 @@ internal sealed class RenderProgressDisplayPanel : Panel
 
 			titleLabel.Text = "Depth Buffer"; //Update title control text
 
+			//Update description control if needed
+			if (descriptionCell.Control is not Label descLabel)
+			{
+				Verbose("Depth Buffer Description Cell {Position} was not Label (was {Control}), disposing and updating", (1, row), depthBufferCell.Control);
+				depthBufferCell.Control?.Detach();
+				depthBufferCell.Control?.Dispose(); //Dispose of the old control
+				statsTable.Add(descLabel = new Label(), 1, row);
+			}
+
+			descLabel.Text = $"{depthBufferBitmap.Width}x{depthBufferBitmap.Height}";
 			//Update image control if needed
 			if (depthBufferCell.Control != depthBufferImageView)
 			{
-				Verbose("Depth Buffer Image Cell {Position} was not our ImageView (was {Control}), disposing and updating", (1, row), depthBufferCell.Control);
+				Verbose("Depth Buffer Image Cell {Position} was not our ImageView (was {Control}), disposing and updating", (2, row), depthBufferCell.Control);
 				depthBufferCell.Control?.Detach();
 				depthBufferCell.Control?.Dispose(); //Dispose of the old control
-				statsTable.Add(depthBufferImageView, 1, row);
+				statsTable.Add(depthBufferImageView, 2, row);
 			}
 
 			depthBufferGraphics.Clear();
@@ -289,25 +369,25 @@ internal sealed class RenderProgressDisplayPanel : Panel
 			//What I'm doing here is adjusting the depth values so that the largest one reaches the end of the graph (scaling up to fill the image)
 			double[] doubleDepths = ArrayPool<double>.Shared.Rent(maxDepth);
 			double   max          = 0;
-			for (int i = 0; i < maxDepth; i++) //Calculate the fractions and the max
+			for (int depth = 0; depth < maxDepth; depth++) //Calculate the fractions and the max
 			{
 				#if true //Toggle whether to use a log function to compress the chart. Mostly needed when we have high max depth values
-				const double b        = 0.000001;
-				double       m        = rayCount;
-				double       fraction = Math.Log((b * renderJob.RenderStats.RawRayDepthCounts[i]) + 1, m) / Math.Log((b * m) + 1, m); //https://www.desmos.com/calculator/erite0if8u
+				const double b        = 800;
+				double       m        = renderStats.RayCount;
+				double       fraction = Math.Log((b * renderStats.RawRayDepthCounts[depth]) + 1, m) / Math.Log((b * m) + 1, m); //https://www.desmos.com/calculator/erite0if8u
 				#else
-				double fraction = renderJob.RawRayDepthCounts[i] / System.Linq.Enumerable.Sum(renderJob.RawRayDepthCounts,u => (double)u); //https://www.desmos.com/calculator/erite0if8u
+				double fraction = renderStats.RawRayDepthCounts[depth] / renderStats.RawRayDepthCounts.Sum(u => (double)u); //https://www.desmos.com/calculator/erite0if8u
 				#endif
 
-				doubleDepths[i] = fraction;
-				max             = Math.Max(max, fraction);
+				doubleDepths[depth] = fraction;
+				max                 = Math.Max(max, fraction);
 			}
 
-			for (int i = 0; i < maxDepth; i++)
+			for (int depth = 0; depth < maxDepth; depth++)
 			{
-				double corrected = doubleDepths[i] / max; //Adjust so the max == 1
+				double corrected = doubleDepths[depth] / max; //Adjust so the max == 1
 				int    endX      = (int)Math.Min(corrected * DepthImageWidth, DepthImageWidth);
-				depthBufferGraphics.DrawLine(depthBufferPen, 0, i, endX, i);
+				depthBufferGraphics.DrawLine(depthBufferPen, 0, depth, endX, depth);
 			}
 
 			//Flush the image or it might not be drawn
@@ -316,15 +396,43 @@ internal sealed class RenderProgressDisplayPanel : Panel
 
 		prevUpdateStats = renderJob.RenderStats;
 
-		static string FormatU(ulong val, ulong total)
+	#region Format Methods
+
+		const string percentFormat = "p1"; //Format string for percentages
+		const string numFormat     = "n0";
+		const int    numAlign      = 15;
+		const int    percentAlign  = 8;
+
+		static string FormatTime(TimeSpan val)
 		{
-			return $"{val.ToString(numFormat),numAlign} {'(' + ((float)val / total).ToString(percentFormat) + ')',percentAlign}";
+			return val.Days != 0 ? val.ToString("d'd 'hh':'mm':'ss'.'f").PadRight(numAlign) : val.ToString("hh':'mm':'ss'.'f").PadRight(numAlign);
 		}
 
-		static string FormatI(int val, int total)
+		static string FormatDate(DateTime val)
+		{
+			return val.ToString("G").PadRight(21);
+		}
+
+		static string FormatUlongRatio(ulong val, ulong total)
 		{
 			return $"{val.ToString(numFormat),numAlign} {'(' + ((float)val / total).ToString(percentFormat) + ')',percentAlign}";
 		}
+		//Format ULong
+		static string FormatUlong(ulong val)
+		{
+			return $"{val.ToString(numFormat),numAlign}";
+		}
+
+		static string FormatIntRatio(int val, int total)
+		{
+			return $"{val.ToString(numFormat),numAlign} {'(' + ((float)val / total).ToString(percentFormat) + ')',percentAlign}";
+		}
+		static string FormatInt(int val)
+		{
+			return $"{val.ToString(numFormat),numAlign}";
+		}
+
+	#endregion
 	}
 
 	/// <inheritdoc/>
