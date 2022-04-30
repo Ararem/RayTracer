1a3b62ded899955d242e8befb876976eb18ac4d9
Eternal <rowanradosavmcrae@gmail.com>
2022-05-01 00:09:10 +1000

    Tmp again

--- RayTracer.Core/Hittables/BoundedPlane.cs
+++ RayTracer.Core/Hittables/BoundedPlane.cs
@@ -1,75 +1,44 @@
 using RayTracer.Core.Acceleration;
 using System.Numerics;
 using static System.Numerics.Vector3;
+using static System.MathF;
 
 namespace RayTracer.Core.Hittables;
 
 /// <summary>
 ///  Bounded version of <see cref="InfinitePlane"/>
 /// </summary>
-public record BoundedPlane(Vector3 Point, Vector3 Normal, float BoundU, float BoundV) : Hittable
+public record Quad(Vector3 A, Vector3 B, Vector3 C, Vector3 D) : Hittable
 {
-	public Vector3 U { get; } = CalculateUvVectors(Normal).U;
-	public Vector3 V { get; } = CalculateUvVectors(Normal).V;
-
-	/// <summary>
-	/// Creates two vectors such that they are both perpendicular to themselves and the <paramref name="normal"/>. Both will lie in the plane that has a normal specified by <paramref name="normal"/>
-	/// </summary>
-	/// <param name="normal">Normal of the plane</param>
-	public static (Vector3 U, Vector3 V) CalculateUvVectors(Vector3 normal)
-	{
-		//Thanks to an anonymous user for this answer https://stackoverflow.com/a/18664291
-
-		/*
-		 * Here we try find a vector that's perpendicular to the plane, by crossing a random vector with the normal
-		 * (This outputs a vector that's perpendicular to both, so it's gonna be along the plane)
-		 * If the `rand` vector is parallel to the normal, then this won't work and the `Cross` will return the Zero vector
-		 * The `UnitX` and `UnitY` vectors are semi-arbitrary, I'm just picking two non-parallel vectors
-		*/
-		Vector3 v1         = Cross(normal, UnitY);
-		if (v1 == Zero) v1 = Cross(normal, UnitY);//Fallback if N ∥ UnitX (colinear)
-
-		v1 = Normalize(v1);
-		Vector3 v2 = Normalize(Cross(normal, v1)); //Find a second orthogonal vector from our first
-
-		return (v1, v2);
-	}
-
 	/// <inheritdoc />
-	public override AxisAlignedBoundingBox BoundingVolume { get; } = ?????;
+	public override AxisAlignedBoundingBox BoundingVolume { get; } = AxisAlignedBoundingBox.Encompass(A,B,C,D);
 
 	/// <inheritdoc />
 	public override HitRecord? TryHit(Ray ray, float kMin, float kMax)
 	{
-		//https://www.cs.princeton.edu/courses/archive/fall00/cs426/lectures/raycast/sld017.htm
-		float normDotDir = Dot(ray.Direction, Normal);
-		float t;
-		//If the ray is going parallel to the plane (through it), then the normal and ray direction will be perpendicular
-		if (MathF.Abs(normDotDir) <= 0.001f) //Approx for ==0
-		{
-			t = kMin; //Going inside plane, find lowest point along the ray
-		}
-		else
-		{
-			//Find intersection normally
-			float d = -Dot(Point, Normal);
-			t = -(Dot(ray.Origin, Normal) + d) / normDotDir;
-		}
+		//Again I'm using IQ's code as reference, since nowhere else seems to have a proper implementation
+		//The main problem is how to choose the bounding points and UV coordinates
 
-		//Assert ranges
-		if ((t < kMin) || (t > kMax)) return null;
+		Vector3 ba  = B - A; Vector3 pa = p - A;
+		Vector3 cb  = C - B; Vector3 pb = p - B;
+		Vector3 dc  = D - C; Vector3 pc = p - C;
+		Vector3 ad  = A - D; Vector3 pd = p - D;
+		Vector3 nor = Cross( ba, ad );
 
-		Vector3 worldPoint = ray.PointAt(t);
-		Vector3 localPoint = worldPoint - Point;
-		//Project the points
-		/*
-		 * This
-		 * [x, y, z] =
-		 */
-		Vector2 uv         = Vector2.Zero;   //A problem with uv's is that we can't really map them onto planes which are infinite
-
-		bool outside = normDotDir < 0; //True if hit on the same side as the normal points to
-
-		return new HitRecord(ray, worldPoint, localPoint, Normal, t, outside, uv);
+		float distanceToPlane = Sqrt(
+				Sign(Dot(Cross(ba, nor), pa)) +
+				Sign(Dot(Cross(cb, nor), pb)) +
+				Sign(Dot(Cross(dc, nor), pc)) +
+				Sign(Dot(Cross(ad, nor), pd)) <3.0
+						?
+						Min( Min( Min(
+												dot2(ba *Clamp(Dot(ba, pa) /dot2(ba), 0.0, 1.0) -pa),
+												dot2(cb *Clamp(Dot(cb, pb) /dot2(cb), 0.0, 1.0) -pb) ),
+										dot2(dc *Clamp(Dot(dc, pc) /dot2(dc), 0.0, 1.0) -pc) ),
+								dot2(ad *Clamp(Dot(ad, pd) /dot2(ad), 0.0, 1.0) -pd) )
+						:
+						Dot(nor, pa) *Dot(nor, pa) /dot2(nor) );
 	}
+
+	private static Vector
 }
