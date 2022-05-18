eaab1ae65452f63d4a03af2aee24b3917c54fc47
rrm <48875125+EternalClickbait@users.noreply.github.com>
2022-05-18 17:20:54 +1000

    Capsule.cs

--- RayTracer.Impl/Hittables/Cylinder.cs
+++ RayTracer.Impl/Hittables/Cylinder.cs
@@ -9,12 +9,19 @@ namespace RayTracer.Impl.Hittables;
 /// <summary>A cylinder, defined by two points and a radius around the line segments of those points</summary>
 public sealed class Cylinder : Hittable
 {
+	/// <summary>Centre of the cylinder, halfway between <see cref="P1"/> and <see cref="P2"/></summary>
 	private readonly Vector3 centre;
+
+	/// <summary><see cref="P2"/> - <see cref="P1"/></summary>
 	private readonly Vector3 p2MinusP1;
 
 	/// <summary><see cref="p2MinusP1"/> dotted with itself</summary>
 	private readonly float p2MinusP1Dot2;
 
+	/// <summary><see cref="radiusSquare"/> * <see cref="p2MinusP1Dot2"/></summary>
+	private readonly float radiusSqrTimesP2P1Dot2;
+
+	/// <summary><see cref="Radius"/> squared</summary>
 	private readonly float radiusSquare;
 
 	/// <summary>A cylinder, defined by two points and a radius around the line segments of those points</summary>
@@ -30,9 +37,10 @@ public sealed class Cylinder : Hittable
 		BoundingVolume = new AxisAlignedBoundingBox(Min(p1, p2) - new Vector3(radius), Max(p1, p2) + new Vector3(radius));
 
 		//Cached vars
-		p2MinusP1     = P2 - P1;
-		p2MinusP1Dot2 = Dot(p2MinusP1, p2MinusP1);
-		radiusSquare  = Radius * Radius;
+		p2MinusP1              = P2 - P1;
+		p2MinusP1Dot2          = Dot(p2MinusP1, p2MinusP1);
+		radiusSquare           = Radius       * Radius;
+		radiusSqrTimesP2P1Dot2 = radiusSquare * p2MinusP1Dot2;
 	}
 
 	/// <inheritdoc/>
@@ -50,9 +58,8 @@ public sealed class Cylinder : Hittable
 	/// <inheritdoc/>
 	public override HitRecord? TryHit(Ray ray, float kMin, float kMax)
 	{
-		//XYZ are XYZ of normal, W is k value along ray of intersection
-		Vector4 kNor;
-
+		float   k;
+		Vector3 normal;
 		Vector3 oc = ray.Origin - P1;
 
 		float bard = Dot(p2MinusP1, ray.Direction);
@@ -60,7 +67,7 @@ public sealed class Cylinder : Hittable
 
 		float k2 = p2MinusP1Dot2 - (bard * bard);
 		float k1 = (p2MinusP1Dot2        * Dot(oc, ray.Direction)) - (baoc * bard);
-		float k0 = (p2MinusP1Dot2        * Dot(oc, oc))            - (baoc * baoc) - (Radius * Radius * p2MinusP1Dot2);
+		float k0 = (p2MinusP1Dot2        * Dot(oc, oc))            - (baoc * baoc) - radiusSqrTimesP2P1Dot2;
 
 		float h = (k1 * k1) - (k2 * k0);
 		if (h < 0.0)
@@ -70,56 +77,50 @@ public sealed class Cylinder : Hittable
 		else
 		{
 			h = Sqrt(h);
-			float t = (-k1 - h) / k2;
+			k = (-k1 - h) / k2;
 
 			// body
-			float y = baoc + (t * bard);
+			float y = baoc + (k * bard);
 			if ((y > 0.0) && (y < p2MinusP1Dot2))
 			{
-				kNor = new Vector4(((oc + (t * ray.Direction)) - ((p2MinusP1 * y) / p2MinusP1Dot2)) / Radius, t);
+				normal = ((oc + (k * ray.Direction)) - ((p2MinusP1 * y) / p2MinusP1Dot2)) / Radius;
 			}
 			// caps
 			else
 			{
-				t = ((y < 0.0f ? 0.0f : p2MinusP1Dot2) - baoc) / bard;
-				if (Abs(k1 + (k2 * t)) < h) kNor = new Vector4((p2MinusP1 * Sign(y)) / p2MinusP1Dot2, t);
+				k = ((y < 0.0f ? 0.0f : p2MinusP1Dot2) - baoc) / bard;
+				if (Abs(k1 + (k2 * k)) < h) normal = (p2MinusP1 * Sign(y)) / p2MinusP1Dot2;
 				else return null;
 			}
 		}
 
-		float k = kNor.W;
 		// ReSharper disable once CompareOfFloatsByEqualityOperator
-		if ((k != -1f) && (k >= kMin) && (k <= kMax))
+		if ((k == -1f) || !(k >= kMin) || !(k <= kMax))
 		{
-			Vector3 normal   = new(kNor.X, kNor.Y, kNor.Z);
+			return null;
+		}
+		else
+		{
 			Vector3 worldPos = ray.PointAt(k);
 			Vector3 localPos = worldPos - centre;
 			bool    inside   = Dot(ray.Direction, normal) > 0f; //If the ray is 'inside' the sphere
 
 			return new HitRecord(ray, worldPos, localPos, normal, k, !inside, Vector2.Zero); //TODO: UV coords
 		}
-		else
-		{
-			return null;
-		}
 	}
 
 	/// <inheritdoc/>
 	public override bool FastTryHit(Ray ray, float kMin, float kMax)
 	{
-		//XYZ are XYZ of normal, W is k value along ray of intersection
-		Vector4 kNor;
-
-		Vector3 ba = P2         - P1;
+		float   k;
 		Vector3 oc = ray.Origin - P1;
 
-		float baba = Dot(ba, ba);
-		float bard = Dot(ba, ray.Direction);
-		float baoc = Dot(ba, oc);
+		float bard = Dot(p2MinusP1, ray.Direction);
+		float baoc = Dot(p2MinusP1, oc);
 
-		float k2 = baba - (bard * bard);
-		float k1 = (baba        * Dot(oc, ray.Direction)) - (baoc * bard);
-		float k0 = (baba        * Dot(oc, oc))            - (baoc * baoc) - (Radius * Radius * baba);
+		float k2 = p2MinusP1Dot2 - (bard * bard);
+		float k1 = (p2MinusP1Dot2        * Dot(oc, ray.Direction)) - (baoc * bard);
+		float k0 = (p2MinusP1Dot2        * Dot(oc, oc))            - (baoc * baoc) - radiusSqrTimesP2P1Dot2;
 
 		float h = (k1 * k1) - (k2 * k0);
 		if (h < 0.0)
@@ -129,24 +130,17 @@ public sealed class Cylinder : Hittable
 		else
 		{
 			h = Sqrt(h);
-			float t = (-k1 - h) / k2;
+			k = (-k1 - h) / k2;
 
-			// body
-			float y = baoc + (t * bard);
-			if ((y > 0.0) && (y < baba))
+			float y = baoc + (k * bard);
+			if (!(y > 0.0) || !(y < p2MinusP1Dot2))
 			{
-				kNor = new Vector4(((oc + (t * ray.Direction)) - ((ba * y) / baba)) / Radius, t);
-			}
-			// caps
-			else
-			{
-				t = ((y < 0.0f ? 0.0f : baba) - baoc) / bard;
-				if (Abs(k1 + (k2 * t)) < h) kNor = new Vector4((ba * Sign(y)) / baba, t);
-				else return false;
+				k = ((y < 0.0f ? 0.0f : p2MinusP1Dot2) - baoc) / bard;
+				if (!(Abs(k1 + (k2 * k)) < h))
+					return false;
 			}
 		}
 
-		float k = kNor.W;
 		// ReSharper disable once CompareOfFloatsByEqualityOperator
 		return (k != -1f) && (k >= kMin) && (k <= kMax);
 	}
