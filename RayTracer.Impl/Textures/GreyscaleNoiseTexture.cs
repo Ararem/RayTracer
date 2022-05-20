74ffbd56bb239325fa96f6c602831f78b865820c
rrm <48875125+EternalClickbait@users.noreply.github.com>
2022-05-20 11:01:21 +1000

    GreyscaleNoiseTexture.cs

--- RayTracer.Impl/Textures/GreyscaleNoiseTexture.cs
+++ RayTracer.Impl/Textures/GreyscaleNoiseTexture.cs
@@ -4,9 +4,18 @@ using SharpNoise.Modules;
 namespace RayTracer.Impl.Textures;
 
 /// <summary>A texture that outputs a greyscale colour depending on the noise value of the noise generator <see cref="Module"/></summary>
-/// <param name="Module">Noise generator module used to generate the colour values</param>
-public record GreyscaleNoiseTexture(Module Module) : Texture
+public sealed class GreyscaleNoiseTexture : Texture
 {
+	/// <summary>A texture that outputs a greyscale colour depending on the noise value of the noise generator <see cref="Module"/></summary>
+	/// <param name="module">Noise generator module used to generate the colour values</param>
+	public GreyscaleNoiseTexture(Module module)
+	{
+		Module = module;
+	}
+
+	/// <summary>Noise generator module used to generate the colour values</summary>
+	public Module Module { get; }
+
 	/// <inheritdoc/>
 	public override Colour GetColour(HitRecord hit) => new((float)Module.GetValue(hit.WorldPoint.X, hit.WorldPoint.Y, hit.WorldPoint.Z));
 }
