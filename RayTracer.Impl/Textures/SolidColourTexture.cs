d072948d6c643fcdba5f253129eb1fb9dd6874d3
rrm <48875125+EternalClickbait@users.noreply.github.com>
2022-05-20 11:02:34 +1000

    SolidColourTexture.cs

--- RayTracer.Impl/Textures/SolidColourTexture.cs
+++ RayTracer.Impl/Textures/SolidColourTexture.cs
@@ -3,8 +3,19 @@ using RayTracer.Core;
 namespace RayTracer.Impl.Textures;
 
 /// <summary>A texture that is a solid colour</summary>
-public sealed record SolidColourTexture(Colour Colour) : Texture
+public sealed class SolidColourTexture : Texture
 {
+	/// <summary>A texture that is a solid colour</summary>
+	public SolidColourTexture(Colour colour)
+	{
+		Colour = colour;
+	}
+
+	/// <summary>
+	/// Colour of the texture
+	/// </summary>
+	public Colour Colour { get; }
+
 	/// <inheritdoc/>
 	public override Colour GetColour(HitRecord _) => Colour;
 }
