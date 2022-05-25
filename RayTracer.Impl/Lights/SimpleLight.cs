d28799ea691b5cb5576262e45acd77c28bae8ff6
rrm <48875125+EternalClickbait@users.noreply.github.com>
2022-05-25 11:04:41 +1000

    Modify SimpleLight.cs a bit more

--- RayTracer.Impl/Lights/SimpleLight.cs
+++ RayTracer.Impl/Lights/SimpleLight.cs
@@ -4,10 +4,13 @@ using System.Numerics;
 namespace RayTracer.Impl.Lights;
 
 //TODO: Cone/Spot lights
+/// <summary>
+/// Simple light that can be coloured, positioned and have it's brightness falloff adjusted. Supports overriding for diffuse lights that cover a volume
+/// </summary>
 public class SimpleLight : Light
 {
 	/// <summary>Colour of the emitted light</summary>
-	public Colour Colour { get; }
+	public Colour Colour { get; init; } = Colour.White;
 
 	/// <summary>The 3D position of the light in world-space</summary>
 	public Vector3 Position { get; }
@@ -17,7 +20,12 @@ public class SimpleLight : Light
 	///  The exact way this is used depends on what function is used to calculate attenuation (see <see cref="DistanceAttenuationFunc"/>). By convention however, the
 	///  light <i>should</i> be significantly attenuated after this point to the point where it is not noticeable
 	/// </remarks>
-	public float Radius { get; }
+	public float Radius { get; init; } = 1f;
+
+	// /// <summary>
+	// /// Float parameter that controls how important it is for the
+	// /// </summary>
+	// public float SurfaceDirectionImportance { get; init; }
 
 
 #region Attenuation things
@@ -61,10 +69,43 @@ public class SimpleLight : Light
 	public static DistanceAttenuationDelegate RealInverseSquareDistanceAttenuation() => static (_, distance) => 1f / (distance * distance);
 
 	/// <inheritdoc cref="DistanceAttenuationDelegate"/>
-	public DistanceAttenuationDelegate DistanceAttenuationFunc { get; }
+	public DistanceAttenuationDelegate DistanceAttenuationFunc { get; init; } = RealInverseSquareDistanceAttenuation();
 
 	/// <summary>Delegate used to calculate how much the intensity of the light should be attenuated at a given <paramref name="distance"/></summary>
 	public delegate float DistanceAttenuationDelegate(SimpleLight light, float distance);
 
 #endregion
+
+	/// <summary>
+	/// Returns an position in world-space, that is used to check if there is an intersection between the hit and the returned point.
+	/// </summary>
+	/// <param name="hit">Information about the hit that will be checked. May be useful for biasing towards the closest point</param>
+	public virtual Vector3 ChooseIntersectTestPosition(HitRecord hit) => Position;
+
+	/// <inheritdoc />
+	public override Colour CalculateLight(HitRecord hit)
+	{
+		//Choose a point to test for intersection
+		//Only applicable to diffuse lights, but doesn't harm point lights
+		Vector3 pointToCheck = ChooseIntersectTestPosition(hit);
+
+		//Check intersection
+		bool intersection = CheckIntersection(hit, pointToCheck, out Ray shadowRay);
+		if(intersection) return Colour.Black;
+
+		Colour colour    = Colour;
+
+		//Account for how much the surface points towards our light
+		float  dot       = Vector3.Dot(shadowRay.Direction, hit.Normal);
+		if (dot < 0) dot = -dot;                                        //Backfaces give negative dot product
+		// dot = MathUtils.Lerp(1, dot, SurfaceDirectionImportance);
+		colour *= dot;
+
+		//Also account for distance attenuation
+		float dist      = Vector3.Distance(hit.WorldPoint, pointToCheck);
+		float distScale = DistanceAttenuationFunc(this, dist);
+		colour *= distScale;
+
+		return colour;
+	}
 }
