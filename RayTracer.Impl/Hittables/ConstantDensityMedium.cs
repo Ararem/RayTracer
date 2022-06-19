c483f1d01610318c07c6cb8abf8fe147ac346c7c
rrm <48875125+EternalClickbait@users.noreply.github.com>
2022-06-19 10:08:50 +1000

    Modify how volumes are handled, attempt to introduce beer's law

--- RayTracer.Impl/Hittables/ConstantDensityMedium.cs
+++ RayTracer.Impl/Hittables/ConstantDensityMedium.cs
@@ -2,7 +2,6 @@
 
 using RayTracer.Core;
 using RayTracer.Core.Acceleration;
-using RayTracer.Impl.Materials;
 using System.Numerics;
 
 #endregion
@@ -13,10 +12,8 @@ namespace RayTracer.Impl.Hittables;
 ///  A hittable that has a constant density (aka a volume like a cloud). Should be used in conjunction with a <see cref="VolumetricMaterial"/>
 /// </summary>
 /// <remarks>This probably won't work too well with rays inside the medium, or other objects inside it, so beware...</remarks>
-public sealed class ConstantDensityMedium
+public sealed class ConstantDensityMedium : Hittable
 {
-	#error FIX CDM Shader communication & material things
-
 	/// <summary>-1 / <see cref="Density"/></summary>
 	private readonly float negInvDensity;
 
@@ -27,34 +24,37 @@ public sealed class ConstantDensityMedium
 		Boundary      = boundary;
 		Density       = density;
 		negInvDensity = -1f / density;
+		Material      = new VolumetricMaterial(Colour.HalfGrey, density);
 	}
+
+	public VolumetricMaterial Material { get; }
 
 	/// <inheritdoc/>
 	public override HitRecord? TryHit(Ray ray, float kMin, float kMax)
 	{
 		//Where we enter the volume
-		if (Boundary.TryHit(ray, kMin, kMax) is not { } hit1) return null;
+		if (Boundary.TryHit(ray, kMin, kMax) is not {} entryHit) return null;
 		//Where we exit the volume
-		if (Boundary.TryHit(ray, hit1.K + 0.001f, kMax) is not { } hit2) return null;
+		if (Boundary.TryHit(ray, entryHit.K + 0.001f, kMax) is not {} exitHit) return null;
 
 		//Find how far we travelled between the inside and outside
-		float boundaryDistance = hit2.K - hit1.K;
-		//Random distance for how far we're going to travel this time
-		float randomHitDistance = negInvDensity * MathF.Log(RandUtils.RandomFloat01());
+		float distanceBetweenHits = exitHit.K - entryHit.K;
+		//Random distance for how far we're going to travel this time (how far along the ray will we intersect with the medium)
+		float distanceToIntersection = negInvDensity * MathF.Log(RandUtils.RandomFloat01());
 
-		if (randomHitDistance > boundaryDistance) return null;
+		if (distanceToIntersection > distanceBetweenHits) return null; //If we intersect outside the boundary, nothing happens
 
 		// ReSharper disable InlineTemporaryVariable
-		Vector3    worldPoint = ray.PointAt(randomHitDistance);
-		Vector3    localPoint = worldPoint;
+		Vector3    worldPoint = ray.PointAt(distanceToIntersection);
+		Vector3    localPoint = worldPoint;                     //Arbitrary
 		Vector3    normal     = RandUtils.RandomOnUnitSphere(); //Arbitrary
-		const bool frontFace  = true;                           //Arbitrary too
+		const bool frontFace  = true;                           //Arbitrary
 
-		return new HitRecord(ray, worldPoint, localPoint, normal, hit1.K + randomHitDistance, frontFace, Vector2.Zero, Material);
+		//Here we pass in how far we travelled 'inside' the object onto the VolumetricMaterial (the shader)
+		return new HitRecord(ray, worldPoint, localPoint, normal, entryHit.K + distanceToIntersection, frontFace, Vector2.Zero, Material, distanceToIntersection);
 	}
 
-
-	/// <summary>How dense the medium is. Higher values increase the change of collision</summary>
+	/// <summary>How dense the medium is. Higher values increase the chance of collision</summary>
 	public float Density { get; }
 
 	/// <summary>The 'shape' of this medium. Can be any shape other than a <see cref="ConstantDensityMedium"/></summary>
@@ -65,4 +65,54 @@ public sealed class ConstantDensityMedium
 
 	/// <inheritdoc/>
 	public override AxisAlignedBoundingBox BoundingVolume => Boundary.BoundingVolume;
+
+	/// <summary>
+	///  A material that scatters in all directions. Should only be used when paired with a volumetric mesh, such as a
+	///  <see cref="ConstantDensityMedium"/>
+	/// </summary>
+	/// <remarks>Scatter direction is completely random</remarks>
+	public sealed class VolumetricMaterial : Material
+	{
+		/// <summary>Colour of the material</summary>
+		public readonly Colour Albedo;
+
+		/// <summary>How dense the material is</summary>
+		public readonly float Density;
+
+		internal VolumetricMaterial(Colour albedo, float density)
+		{
+			Albedo  = albedo;
+			Density = density;
+		}
+
+		/// <inheritdoc/>
+		public override Ray? Scatter(HitRecord hit, ArraySegment<HitRecord> previousHits) => new Ray(hit.WorldPoint, RandUtils.RandomOnUnitSphere());
+
+		/// <inheritdoc/>
+		public override Colour CalculateColour(Colour colour, HitRecord hit, ArraySegment<HitRecord> previousHits)
+		{
+			/*
+			 * D = 1, A = .5
+			 * x=1: out = .5
+			 * x=2: out = .5*.5 = .25
+			 * x=3: out = .125
+			 *
+			 * out = (A) ^ x for D = 1
+			 *
+			 * D = 2, A = .5
+			 * x=.5: out = .5
+			 * x=1: out = .25
+			 *
+			 * out = (A) ^ (D * x) for any D
+			 */
+			//Calculate how much the colour is attenuated
+			float distanceInside = hit.ShaderData as float? ?? throw new InvalidShaderDataException(hit.ShaderData, $"hit.ShaderData was not a float (was {hit.ShaderData?.GetType()}: {hit.ShaderData})");
+			float pow            = Density * distanceInside;
+			(float albedoR, float albedoG, float albedoB) = Albedo;
+			albedoR                           = MathF.Pow(albedoR, pow);
+			albedoG                           = MathF.Pow(albedoG, pow);
+			albedoB                           = MathF.Pow(albedoB, pow);
+			return new Colour(albedoR, albedoG, albedoB) * colour;
+		}
+	}
 }
