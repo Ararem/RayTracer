c444143c695ad2c5050e53334f7a6213fbdb6427
rrm <48875125+EternalClickbait@users.noreply.github.com>
2022-07-22 12:08:11 +1000

    Add assembly info attributes

---
+++ Ararem.RayTracer.Display.Dev/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using static Ararem.RayTracer.Core.AssemblyInfo;
+
+[assembly: AssemblyDescription(Description)]
+[assembly: AssemblyConfiguration(Configuration)]
+[assembly: AssemblyProduct(ProductName)]
+[assembly: AssemblyTitle(DevAppName)]
+[assembly: AssemblyVersion(Version)]
+[assembly: AssemblyFileVersion(Version)]
+[assembly: AssemblyInformationalVersion(Version)]
---
+++ Ararem.RayTracer.Impl/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using Ararem.RayTracer.Core;
+using System.Reflection;
+using static Ararem.RayTracer.Core.AssemblyInfo;
+
+[assembly: AssemblyDescription(Description)]
+[assembly: AssemblyConfiguration(Configuration)]
+[assembly: AssemblyProduct(ProductName)]
+[assembly: AssemblyTitle(DevAppName)]
+[assembly: AssemblyVersion(AssemblyInfo.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfo.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfo.Version)]
